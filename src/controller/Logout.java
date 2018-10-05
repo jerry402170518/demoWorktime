b9bfe1d69091d5fbd746016a91ec68bab420ca79
JerryLin <serious830915@gmail.com>
2018-10-05 21:51:38 +0800

    變更密碼:新增alert與登出完成

---
+++ src/controller/Logout.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final String LOGOUT_PAGE="login.jsp";
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if (request.getSession().getAttribute("login") != null) {  
+			request.getSession().removeAttribute("login");
+			request.getSession().invalidate();
+		    response.sendRedirect(LOGOUT_PAGE);
+		    return; 
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
